60e5f3ac084347419e4fb7dd184c3c4118d533e0
yangjp22 <jinyang0604@gmail.com>
2024-09-25 00:08:38 -0400

    Add app for lists, with deliberately failing unit test

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://127.0.0.1:8000/")
         self.assertIn("Congratulation", self.browser.title)
-        self.fail("Intentional failure")
+        self.fail("Intentional failure 1")
 
 
 if __name__ == "__main__":
