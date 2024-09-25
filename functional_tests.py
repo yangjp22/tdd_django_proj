63403840124facfeca542690fb423e5ef153f513
yangjp22 <jinyang0604@gmail.com>
2024-09-24 22:34:20 -0400

    initial try

---
+++ functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        self.assertIn("Congratulation", self.browser.title)
+        self.fail("Intentional failure")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
